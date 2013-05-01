dedf53c41c60022625f525ced45baae0310f88c0
ArneMR <arne.richter@haw-hamburg.de>
2013-05-02 00:48:41 +0200

    Removed all echos
    http://doc.owncloud.com/server/5.0/developer_manual/security.html

--- templates/settings.php
+++ templates/settings.php
@@ -2,36 +2,36 @@
     <fieldset class="personalblock">
         <legend><strong>Redmine</strong></legend>
         <p>
-            <label for="redmine_db_host"><?php echo $l->t('DB Host');?></label>
+            <label for="redmine_db_host"><?php p($l->t('DB Host'));?></label>
             <input type="text" id="redmine_db_host" name="redmine_db_host"
-                value="<?php echo $_['redmine_db_host']; ?>" />
+                value="<?php p($_['redmine_db_host']); ?>" />
 
-            <label for="redmine_db_name"><?php echo $l->t('DB Name');?></label>
+            <label for="redmine_db_name"><?php p($l->t('DB Name'));?></label>
             <input type="text" id="redmine_db_name" name="redmine_db_name" 
-                value="<?php echo $_['redmine_db_name']; ?>" />
+                value="<?php p($_['redmine_db_name']); ?>" />
 
-            <label for="redmine_db_driver"><?php echo $l->t('DB Driver');?></label>
+            <label for="redmine_db_driver"><?php p($l->t('DB Driver'));?></label>
             <?php $db_driver = array('mysql' => 'MySQL', 'pgsql' => 'PostgreSQL');?>
             <select id="redmine_db_driver" name="redmine_db_driver">
                 <?php foreach ($db_driver as $driver => $name): ?>
-                    <?php echo $_['redmine_db_driver']; ?>
+                    <?php p($_['redmine_db_driver']); ?>
                     <?php if ($_['redmine_db_driver'] === $driver): ?>
-                        <option selected="selected" value="<?php echo $driver; ?>"><?php echo $name; ?></option>
+                        <option selected="selected" value="<?php p($driver); ?>"><?php p($name); ?></option>
                     <?php else: ?>
-                        <option value="<?php echo $driver; ?>"><?php echo $name; ?></option>
+                        <option value="<?php p($driver); ?>"><?php p($name); ?></option>
                     <?php endif ?>
                 <?php endforeach; ?>
             </select>
         </p>
 
         <p>
-            <label for="redmine_db_user"><?php echo $l->t('DB User');?></label>
+            <label for="redmine_db_user"><?php p($l->t('DB User'));?></label>
             <input type="text" id="redmine_db_user" name="redmine_db_user" 
-                value="<?php echo $_['redmine_db_user']; ?>" />
+                value="<?php p($_['redmine_db_user']); ?>" />
 
-            <label for="redmine_db_password"><?php echo $l->t('DB Password');?></label>
+            <label for="redmine_db_password"><?php p($l->t('DB Password'));?></label>
             <input type="password" id="redmine_db_password" name="redmine_db_password" 
-                value="<?php echo $_['redmine_db_password']; ?>" />
+                value="<?php p($_['redmine_db_password']); ?>" />
         </p>
 
         <input type="submit" value="Save" />
